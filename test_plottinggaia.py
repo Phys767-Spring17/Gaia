f8666aec0a271c05f61c3134cb96dc00f6ba82a3
drewjrosen <drew.j.rosen@gmail.com>
2017-05-19 16:36:05 -0400

    Add travis test file

---
+++ test_plottinggaia.py
@@ -0,0 +1,23 @@
+from plottinggaia import plottinggaia
+import pytest
+
+
+#travis tests
+#these are independent of the tests already written in the prgram
+
+#idiot test
+def test_highsnobjects1()
+
+	assert 1==1
+
+
+#make sure we have positive distances
+def test_highsnobjects2()
+	
+	assert distance >= 0.
+
+
+#make sure band sizes match so they can subtract correctly to calculate color
+def test_highsnobjects3()
+
+	assert len(j_band)==len(k_band)
